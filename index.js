fcfa8d8d8c566ea6fab9ba0e22c50580054977cf
Alexander <alex.galev@yahoo.com>
2023-02-07 11:08:26 -0700

    solved

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Write your code here...
+// Getting a menu list of items looking like:
+// {
+//     "id": 1,
+//     "name": "Chips & Guacamole",
+//     "image": "./assets/calexico-guac.webp",
+//     "description":"House-made tortilla chips with fresh daily made guacamole",
+//     "price": 13.00,
+//     "number_in_bag": 0
+// }
+
+const menuList = document.getElementById('menu-items')
+const image = document.getElementById('dish-image')
+const name = document.getElementById('dish-name')
+const description = document.getElementById('dish-description')
+const price = document.getElementById('dish-price')
+const cartNumber = document.getElementById('number-in-cart')
+const amount = document.getElementById('cart-amount')
+const formSubmit = document.getElementById('cart-form')
+formSubmit.addEventListener('submit', (event) => addToCart(event))
+
+const fetchMenu = () => {
+	fetch('http://localhost:3000/menu')
+		.then((res) => res.json())
+		.then((menu) => {
+			getMenuItems(menu)
+			dish(menu[0])
+		})
+}
+
+const getMenuItems = (menu) => {
+	menu.forEach((item) => {
+		const span = document.createElement('span')
+		span.setAttribute('id', item.id)
+		span.addEventListener('click', () => dish(item))
+		span.textContent = item.name
+		menuList.appendChild(span)
+	})
+}
+
+const dish = (menu) => {
+	image.setAttribute('src', menu.image)
+	name.textContent = menu.name
+	description.textContent = menu.description
+	price.textContent = menu.price
+}
+
+const addToCart = (event) => {
+    event.preventDefault()
+    cartNumber.innerText = parseInt(cartNumber.innerText) + parseInt(event.target[0].value)
+}
+
+fetchMenu()
